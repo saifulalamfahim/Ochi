480ea2f22b49e6ca368400abb4ca60d9503643a5
Saiful Alam Fahim <fahim12345saiful@gmail.com>
2024-02-14 16:56:04 +0600

    navbar

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      Neue: ['Bebas Neue', 'sans-serif'],
+     },
+    extend: {},
+  },
+  plugins: [],
+}
+
